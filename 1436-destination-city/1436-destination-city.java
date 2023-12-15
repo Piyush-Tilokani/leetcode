6387b7890092e9fd532c94fba46ac6fb9266bfd5
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2023-12-15 20:19:05 +0530

    Time: 2 ms (79.52%), Space: 43.1 MB (39.87%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,33 @@
+// class Solution {
+//     public String destCity(List<List<String>> paths) {
+//         List<String> srcs = new ArrayList<>();
+//         for (List<String> path: paths ){
+//             srcs.add(path.get(0));
+//         }
+//         for (List<String> path: paths ){
+//             if(!srcs.contains(path.get(1))){
+//                 return path.get(1);
+//             }
+//         }
+//         System.gc();
+//         return "";
+//     }
+// }
+
+//More optimised approach (Using sets)
+
+class Solution{
+     public String destCity(List<List<String>> paths) {
+         HashSet<String> srcs = new HashSet<String>();
+         for(List<String> path: paths){
+             srcs.add(path.get(0));
+         }
+         for (List<String> path: paths ){
+            if(!srcs.contains(path.get(1))){
+                return path.get(1);
+            }
+        }
+         // System.gc();
+        return "";
+     }
+}
