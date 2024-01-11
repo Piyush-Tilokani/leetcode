8cab20b3de35210d6a1c77b167994dcb48e23c14
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-01-12 00:08:31 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (13.31%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.java
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public int alternateDigitSum(int n) {
-        int digits = 0;
-        int ans = 0;
+        int digits = 0; //To count the number of digits in n
+        int ans = 0; //The final answer
         while(n != 0){
-            ans += (Math.pow(-1, (digits++))) * n%10;    
+            ans += (Math.pow(-1, (digits++))) * n%10;  //Math.pow determines the sign of digit at each step
             n /= 10;
         }
-        return digits%2 == 0 ? -1*ans : ans;
+        return digits%2 == 0 ? -1*ans : ans; //If the number of digits is even, we return -ve of ans, else we simply return the answer
     }
 }
