8d27ad00e45880f258da808a2d3e5c9df3dfb728
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-04-15 00:55:41 +0530

    Time: 5 ms (100.00%), Space: 78.7 MB (100.00%) - LeetHub

---
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.java
@@ -0,0 +1,27 @@
+class Solution {
+    private boolean isPrime(int n){
+        if(n<2)
+            return false;
+        if(n==2)
+            return true;
+        for(int i=2; i*i <=n; i++){
+             if(n%i==0)
+                 return false;
+        }
+        return true;
+    }
+    public int maximumPrimeDifference(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        System.out.println(r+1);
+        while(l<=r){
+            if(!isPrime(nums[l]))
+                l++;
+            if(!isPrime(nums[r]))
+                r--;
+            if(isPrime(nums[l]) && isPrime(nums[r]))
+                return r-l;
+        }
+        return 0;
+    }
+}
