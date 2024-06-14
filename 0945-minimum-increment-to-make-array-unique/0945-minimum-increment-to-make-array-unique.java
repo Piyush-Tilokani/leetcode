f688b87af1280f900ce75af7fd62207c4649cb23
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-06-15 01:33:23 +0530

    Time: 4 ms (99.72%), Space: 60.3 MB (29.06%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+
+        //Basic sorting approach TC: O(n logn), SC: O(1)
+        //37ms
+        
+        // Arrays.sort(nums);
+        // int ans=0;
+        // for(int i=0; i<nums.length-1; i++){
+        //     if(nums[i+1]<=nums[i]){
+        //         ans += (nums[i] + 1) - nums[i+1];
+        //         nums[i+1]=nums[i]+1;
+        //     }
+        // }
+        // return ans;
+
+        //-----------------------------------------------------------------------------------------------------
+
+        //Count sort with hashmap TC: O(n + max(nums)), SC: O(n + max(nums))
+        //80ms
+        
+        // int n = nums.length;
+        // int max=0;
+        // int ans=0;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int i: nums){
+        //     max=Math.max(i, max);
+        //     map.put(i, 1+map.getOrDefault(i,0));
+        // }
+        // for(int i=0; i<n+max; i++){
+        //     if(map.getOrDefault(i, 0)>1){
+        //         ans+=map.get(i)-1;
+        //         map.put(i+1, map.getOrDefault(i+1, 0) + map.get(i)-1);
+        //     }
+        // }
+        // return ans;
+
+        //-----------------------------------------------------------------------------------------------------
+
+        //Same count sort but with array as map TC: O(n + max(nums)), SC: O(n + max(nums))
+        //Faster runtime - 5ms
+
+        int max=0;
+        int ans=0;
+        for(int i: nums){
+            max=Math.max(i, max);
+        }
+        int freq[]=new int[max+nums.length];
+        for(int i: nums)
+            freq[i]++;
+        // System.out.println("Initially map: "+map);
+        for(int i=0; i<freq.length; i++){
+            if(freq[i]>1){
+                ans+=freq[i]-1;
+                freq[i+1] += freq[i]-1;
+            }
+        }
+        return ans;
+    }
+}
