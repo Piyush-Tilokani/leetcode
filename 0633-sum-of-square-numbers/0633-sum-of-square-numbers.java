934665d9a30797408665d875ccd02d6084507610
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-06-17 16:49:47 +0530

    Time: 106 ms (5.11%), Space: 40.7 MB (9.53%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,33 @@
+class Solution {
+    private boolean isPerfectSq(int num){ //Simple Binary search implementation  https://leetcode.com/problems/valid-perfect-square/
+        int l=1; 
+        int r=num;
+        while(l<r){
+            long m = l + ((r-l)/2);
+            if(m*m == num)
+                return true;
+            else if(m*m > num)
+                r = (int)m-1;
+            else
+                l = (int)m+1;
+        }
+        return false;
+    }
+    public boolean judgeSquareSum(int c) {
+        if(isPerfectSq(c)){
+            System.out.println("returning from sqrt");
+            return true;
+        }
+        long l=0;
+        long r=(int)Math.sqrt(c);
+        while(l<=r){
+            if((l*l) + (r*r) == c)
+                return true;
+            else if((l*l) + (r*r) > c)
+                r--;
+            else
+                l++;
+        }
+        return false;
+    }
+}
