7618f4df58d7e73779ad3c99a5e08c8194e0a4b0
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-06-21 00:56:48 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.53%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -24,14 +24,32 @@ class Solution {
     public void flatten(TreeNode root) {
         //Brute force: Store the preorder traversal in a queue and then make the connections
         //TC: O(n), SC:O(n)
-        Queue<TreeNode> q = new LinkedList<>();
-        preorder(root, q);
-        TreeNode current = q.poll();
-        while(!q.isEmpty()){
-            current.left = null;
-            current.right = q.poll();
-            current = current.right;
-        }
+        // Queue<TreeNode> q = new LinkedList<>();
+        // preorder(root, q);
+        // TreeNode current = q.poll();
+        // while(!q.isEmpty()){
+        //     current.left = null;
+        //     current.right = q.poll();
+        //     current = current.right;
 
+
+        //--------------------------------------------------------------------------------------------------
+
+
+        //Optimal solution: At every point, join the right subtree to rightmost empty spot of left subtree
+        //join the right subtree to left and make left null
+        //TC: O(n), SC:O(1)
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left!=null){ //If left is not null, adjust left part
+                TreeNode temp = curr.left;  
+                while(temp.right!=null) //Find the rightmost empty spot in the left tree
+                    temp=temp.right;
+                temp.right = curr.right; //Join current tree's right subtree to right of the empty spot
+                curr.right = curr.left;  //Reassign right of current node to adjusted left part
+                curr.left = null;        //Make left of the current node null
+            }
+            curr=curr.right; //If left is null or already adjustded, move to right node
+        }
     }
 }
