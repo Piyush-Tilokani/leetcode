1e9879c5466c13a3737ff128661c1bdb587fbf8f
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-07-07 03:01:54 +0530

    Time: 2 ms (27.74%), Space: 57.7 MB (9.97%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,11 +4,35 @@ class Solution {
             return;
         //Copying into new array
         //TC: O(n), SC: O(n)
-        int copy[] = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            copy[(i+k)%nums.length] = nums[i];
+        // int copy[] = new int[nums.length];
+        // for(int i=0; i<nums.length; i++){
+        //     copy[(i+k)%nums.length] = nums[i];
+        // }
+        // for(int i=0; i<nums.length; i++)
+        //     nums[i] = copy[i];
+
+        //Reversing the array and then reversing the first k and the rest of the Array
+        //TC: O(N), SC: O(1) But 3 passes
+        int l=0;
+        int r=nums.length-1;
+        while(l<r){
+            int temp = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = temp;
         }
-        for(int i=0; i<nums.length; i++)
-            nums[i] = copy[i];
+        l=0;
+        r=(k-1)%nums.length; //Adjust right end for over/underflow
+        while(l<r){  //Invert array before pivot
+            int temp = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = temp;
+        }
+        l=k%nums.length;  //Adjust left end for over/underflow
+        r=nums.length-1;
+        while(l<r){  //Invert array after pivot
+            int temp = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = temp;
+        }
     }
 }
