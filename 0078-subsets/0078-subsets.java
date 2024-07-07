3ed4861f097bcbbc52a18700796d629c9b51b3a9
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-07-08 03:43:52 +0530

    Time: 0 ms (100%), Space: 42.8 MB (12.79%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,50 @@
 class Solution {
-    private void getSub(int nums[], int n, List<Integer> l, List<List<Integer>> ans){
-        if(n==nums.length){
-            List<Integer> addMe = new ArrayList<>(l); //This line is important because the ans variable is taking an instance of List<> as inputs
-            ans.add(addMe);
-            return;
-        }
-        getSub(nums, n+1, l, ans);
-        l.add(nums[n]);
-        getSub(nums, n+1, l, ans);
-        l.remove(l.size()-1);
-    }
+    // private void getSub(int nums[], int n, List<Integer> l, List<List<Integer>> ans){
+    //     if(n==nums.length){
+    //         List<Integer> addMe = new ArrayList<>(l); //This line is important because the ans variable is taking an instance of List<> as input
+    //         ans.add(addMe);
+    //         return;
+    //     }
+    //     getSub(nums, n+1, l, ans);
+    //     l.add(nums[n]);
+    //     getSub(nums, n+1, l, ans);
+    //     l.remove(l.size()-1);
+    // }
     public List<List<Integer>> subsets(int[] nums) {
+        //Recursive approach
+        // List<List<Integer>> ans = new ArrayList<>();
+        // getSub(nums, 0, new ArrayList<Integer>(), ans);
+        // return ans;
+
+        //-------------------------------------------------------------
+
+        //Iterative approach
+
+        //Approach:  for n = 3, arr = [1, 2, 3]
+        // 0 = Not pick, 1 = Pick
+        // All possible cases
+        // 0 0 0    --->   If ith, bit is set, add the ith element of array else skip
+        // 0 0 1
+        // 0 1 0
+        // 0 1 1
+        // 1 0 0
+        // 1 0 1
+        // 1 1 0
+        // 1 1 1
+
+
         List<List<Integer>> ans = new ArrayList<>();
-        getSub(nums, 0, new ArrayList<Integer>(), ans);
+        int n = nums.length;
+        int subsets = 1<<n; //Total number of subsets (2^n)
+        for(int i=0; i<subsets; i++){
+            List<Integer> sub = new ArrayList<>(); //Each intermediate subset
+            for(int j=0; j<n; j++){
+                if( (i & (1<<j)) != 0)
+                    sub.add(nums[j]);
+            }
+            ans.add(sub);
+        }
+        // System.out.println(subsets);
         return ans;
     }
 }
