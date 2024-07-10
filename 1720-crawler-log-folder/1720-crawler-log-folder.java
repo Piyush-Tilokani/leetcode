e2483ac80b097c3a9b8738c787ad6c387fbf2759
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-07-10 14:49:51 +0530

    Time: 1 ms (99.42%), Space: 41.8 MB (67.59%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,12 +1,27 @@
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> st = new Stack<>();
+        //Using Stack: TC: O(n), SC: O(n)
+
+        // Stack<String> st = new Stack<>();
+        // for(String l: logs){
+        //     if(l.equals("../") && !st.isEmpty())
+        //         st.pop();
+        //     else if(!(l.equals("./") || l.equals("../")))
+        //         st.push(l);
+        // }
+        // return st.size();
+
+        //-----------------------------------------------------------------------------------------
+
+        //Using counter variable: TC: O(n), SC: O(1)
+
+        int ans = 0; //Depth of folders from main folder
         for(String l: logs){
-            if(l.equals("../") && !st.isEmpty())
-                st.pop();
+            if(l.equals("../") && ans>0)
+                ans--;
             else if(!(l.equals("./") || l.equals("../")))
-                st.push(l);
+                ans++;
         }
-        return st.size();
+        return ans;
     }
 }
