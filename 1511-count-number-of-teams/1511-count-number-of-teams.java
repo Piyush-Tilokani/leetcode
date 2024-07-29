2d8543a9528f46316f0d56b000c6b478ad941062
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-07-30 02:18:37 +0530

    Time: 2156 ms (6.64%), Space: 41.5 MB (100%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        //Brute force: TC: O(n^3), SC: O(1)
+        int ans=0;
+        int n=rating.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    if(rating[i]>rating[j] && rating[j]>rating[k])
+                        ans++;
+                    if(rating[i]<rating[j] && rating[j]<rating[k])
+                        ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
