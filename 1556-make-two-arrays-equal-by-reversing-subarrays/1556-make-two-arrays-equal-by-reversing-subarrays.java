ffc64d96fdf883ffe5091b4b7f40cc469c93baa4
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-08-04 00:35:39 +0530

    Time: 1 ms (100%), Space: 44.1 MB (73.75%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -5,8 +5,8 @@ class Solution {
             freq[arr[i]]++;
             freq[target[i]]--;
         }
-        for(int i: freq){
-            if(i!=0)
+        for(int i=0; i<arr.length; i++){
+            if(freq[target[i]]!=0 || freq[arr[i]]!=0)
                 return false;
         }
         return true;
