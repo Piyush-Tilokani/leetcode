d02bd9e47ab69f592b6a2922b85b6df7168de5ca
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-08-07 01:00:51 +0530

    Time: 9 ms (61.15%), Space: 45.6 MB (64.04%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -5,13 +5,12 @@ class Solution {
         int keys=0;
         for(char c: word.toCharArray())
             freq[c-'a']++;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(freq[b], freq[a]));
-        for(int i=0; i<26; i++)
-            pq.offer(i);
-        while(!pq.isEmpty() && freq[pq.peek()]>0){
+        Arrays.sort(freq);
+        int i=25;
+        while(i>=0 && freq[i]>0){
             keys++;
             int multiplier = keys%8!=0? keys/8 + 1 : keys/8;
-            ans += freq[pq.poll()]*multiplier;            
+            ans += freq[i--]*multiplier;            
         }
         return ans;
     }
