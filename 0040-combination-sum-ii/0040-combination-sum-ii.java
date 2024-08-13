e2023545719a6c532de4315438b6a5fb33126ab5
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-08-13 21:55:26 +0530

    Time: 3 ms (82.44%), Space: 42.9 MB (93.74%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     private void combine(int []nums, int t, int i, List<Integer> l, List<List<Integer>> ans){
-        if(t==0){ //If targer is achieved, add list to ans and return
+        if(t==0){
             ans.add(new ArrayList<>(l));
             return;
-        }        
-        for(int j=i; j<nums.length; j++){ //For each element, check if it can be the jth element of the intermediate list
-            if(j>i && nums[j]==nums[j-1]) //If it is already present at the jth position in recursive call, continue to next element
+        }
+        for(int j=i; j<nums.length; j++){
+            if(j!=i && nums[j]==nums[j-1])
                 continue;
-            if(nums[j]>t) //If element exceeds the target break
+            if(nums[j]>t)   
                 break;
             l.add(nums[j]);
             combine(nums, t-nums[j], j+1, l, ans);
             l.remove(l.size()-1);
         }
-           
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
