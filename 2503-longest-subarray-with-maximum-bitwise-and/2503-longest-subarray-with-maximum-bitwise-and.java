9d8f461569b65026b806a03da7db138ba815761b
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-09-14 16:31:27 +0530

    Time: 3 ms (89.89%), Space: 59.4 MB (55.06%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
@@ -2,20 +2,18 @@ class Solution {
     public int longestSubarray(int[] nums) {
         //Approach: The largest AND sub-array needs to have the larges element in the array
         //So the largest required subaaray is largest sequence of 'max' valued numbers"
-        int max = 0;
-        int ans = 0;
-        int count = 0;
-        for(int i: nums){
-            if(i>max){ //New max found
-                max=i; 
-                ans=0; 
-                count=0;
+        int max=0;
+        for(int i: nums) //Get the max element
+            max=Math.max(max, i);
+        int ans = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==max){
+                int j=i+1;
+                while(j<nums.length && nums[j]==max) //Find the longest sequence of max elements
+                    j++;
+                ans=Math.max(ans, j-i);
+                i=j-1;
             }
-            if(i==max)
-                count++;
-            else
-                count=0;
-            ans=Math.max(ans, count);
         }
         return ans;
     }
