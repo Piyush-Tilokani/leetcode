8aaf81f8f0a6468a4bd9071b736fe72445520a17
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-10-06 20:11:47 +0530

    Time: 1 ms (100%), Space: 45.8 MB (46.79%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.java
@@ -1,27 +1,33 @@
 class Solution {
-    private void helper(int i, int n, String s, List<String> ans){
+    // Approach: Recursive backtracking
+    private void helper(int i, int n, char[] s, List<String> ans){
         if(i==n){
-            ans.add(s);
+            ans.add(new String(s));
             return;
         }
         if(i==0){
-            helper(i+1, n, s+"0", ans);
-            helper(i+1, n, s+"1", ans);
+            s[0] = '0';
+            helper(i+1, n, s, ans);
+            s[0] = '1';
+            helper(i+1, n, s, ans);
             return;
         }
-        if(s.charAt(i-1)=='0'){
-            helper(i+1, n, s+"1", ans);
+        if(s[i-1]=='0'){
+            s[i] = '1';
+            helper(i+1, n, s, ans);
             return;
         }
         else{
-            helper(i+1, n, s+"0", ans);
-            helper(i+1, n, s+"1", ans);
+            s[i] = '0';
+            helper(i+1, n, s, ans);
+            s[i] = '1';
+            helper(i+1, n, s, ans);
             return;
         }
     }
     public List<String> validStrings(int n) {
         List<String> ans = new ArrayList<>();
-        helper(0, n, "", ans);
+        helper(0, n, new char[n], ans);
         return ans;
     }
 }
