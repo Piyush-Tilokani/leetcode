2fa66eb65a57a767e24feab8c07aa18ee5bc9e0f
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-10-17 22:49:56 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66.95%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maximumSwap(int num) {
+        int ans=0;
+        int n=(int)Math.log10(num) + 1;
+        int arr[] = new int[n];
+        int numCopy = num;
+        for(int i=n-1; i>=0; i--){
+            arr[i] = numCopy%10;
+            numCopy/=10;
+        }
+        boolean swap = false;
+        for(int i=0; i<n-1; i++){
+            if(arr[i+1]>arr[i]){
+                int max = arr[i+1];
+                int maxInd = i+1;
+                for(int j=i+1; j<n; j++){
+                    if(arr[j]>=max){
+                        max=arr[j];
+                        maxInd=j;
+                    }
+                }
+                for(int k=0; k<n; k++){
+                    if(arr[k]<max){
+                        int temp = arr[k];
+                        arr[k] = max;
+                        arr[maxInd] = temp;
+                        break;
+                    }
+                }                
+                swap=true;
+                break;
+            }
+        }
+        if(!swap)
+            return num;
+        for(int i=0; i<n; i++){
+            ans+=arr[i];
+            ans*=10;
+        }
+        return ans/10;
+    }
+}
