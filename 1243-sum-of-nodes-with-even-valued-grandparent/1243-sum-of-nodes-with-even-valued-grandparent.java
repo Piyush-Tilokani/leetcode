40c0aedc84fd14bc2930e7203bd22bd429a92fbe
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2024-12-31 21:38:55 +0530

    Time: 3 ms (21.06%), Space: 45.5 MB (94.86%) - LeetHub

---
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans=0;
+    private int secondLevelSum(TreeNode root){
+        if(root==null)
+            return 0;
+        int level = 0;
+        int sum=0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(level<3 && !q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                TreeNode n = q.poll();
+                if(level==2)
+                    sum+=n.val;
+                if(n.left!=null)
+                    q.offer(n.left);
+                if(n.right!=null)
+                    q.offer(n.right);
+            }
+            level++;
+        }
+        return sum;
+    }
+    private void inOrder(TreeNode root){
+        if(root==null)
+            return;
+        inOrder(root.left);
+        if(root.val%2==0){
+            ans+=secondLevelSum(root);
+        }
+        inOrder(root.right);
+    }
+    public int sumEvenGrandparent(TreeNode root) {
+        inOrder(root);
+        return ans;
+    }
+}
