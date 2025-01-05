80b268b0094877e0233ef06cb5f8fa1164d19393
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2025-01-05 21:23:49 +0530

    Time: 1 ms (100%), Space: 46.2 MB (58.92%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -15,34 +15,55 @@
  */
 class Solution {
     int ans=0;
-    private int secondLevelSum(TreeNode root){
-        if(root==null)
-            return 0;
-        int level = 0;
-        int sum=0;
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        while(level<3 && !q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode n = q.poll();
-                if(level==2)
-                    sum+=n.val;
-                if(n.left!=null)
-                    q.offer(n.left);
-                if(n.right!=null)
-                    q.offer(n.right);
-            }
-            level++;
-        }
-        return sum;
-    }
+    // private int secondLevelSum(TreeNode root){
+    //     if(root==null)
+    //         return 0;
+    //     int level = 0;
+    //     int sum=0;
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     q.offer(root);
+    //     while(level<3 && !q.isEmpty()){
+    //         int size = q.size();
+    //         for(int i=0; i<size; i++){
+    //             TreeNode n = q.poll();
+    //             if(level==2)
+    //                 sum+=n.val;
+    //             if(n.left!=null)
+    //                 q.offer(n.left);
+    //             if(n.right!=null)
+    //                 q.offer(n.right);
+    //         }
+    //         level++;
+    //     }
+    //     return sum;
+    // }
+    // private void inOrder(TreeNode root){
+    //     if(root==null)
+    //         return;
+    //     inOrder(root.left);
+    //     if(root.val%2==0){
+    //         ans+=secondLevelSum(root);
+    //     }
+    //     inOrder(root.right);
+    // }
+
     private void inOrder(TreeNode root){
         if(root==null)
             return;
         inOrder(root.left);
         if(root.val%2==0){
-            ans+=secondLevelSum(root);
+            if(root.left!=null){
+                if(root.left.left!=null)
+                    ans+=root.left.left.val;
+                if(root.left.right!=null)
+                    ans+=root.left.right.val;
+            }
+            if(root.right!=null){
+                if(root.right.left!=null)
+                    ans+=root.right.left.val;
+                if(root.right.right!=null)
+                    ans+=root.right.right.val;
+            }
         }
         inOrder(root.right);
     }
