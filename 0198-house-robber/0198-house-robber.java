0719aa1deea962188973a6d90e23d06555f3ef9d
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2025-01-28 22:33:34 +0530

    Time: 0 ms (100%), Space: 41.5 MB (8.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -12,21 +12,36 @@ class Solution {
     // }
 
     //Topdown dp (Memoization)
+    //TC: O(n), SC: O(n) + O(n) (Stack space for recursion)
 
-    private int rob(int [] nums, int i, int [] dp){
-        if(i<0)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int pick=nums[i]+rob(nums, i-2, dp);
-        int notPick=rob(nums, i-1, dp);
-        dp[i]=Math.max(pick, notPick);
-        return dp[i];
-    }
+    // private int rob(int [] nums, int i, int [] dp){
+    //     if(i<0)
+    //         return 0;
+    //     if(dp[i]!=-1)
+    //         return dp[i];
+    //     int pick=nums[i]+rob(nums, i-2, dp);
+    //     int notPick=rob(nums, i-1, dp);
+    //     dp[i]=Math.max(pick, notPick);
+    //     return dp[i];
+    // }
+
+
+   
 
     public int rob(int[] nums) {
         int []dp=new int[nums.length];
-        Arrays.fill(dp, -1);
-        return rob(nums, nums.length-1, dp);
+        // Arrays.fill(dp, -1);
+
+        //Bottom-up dp (Tabulation)
+        //TC: O(n), SC: O(n) (Stack space reduced)
+        dp[0]=nums[0];
+        for(int i=1; i<nums.length; i++){
+            int pick=nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            int notPick=dp[i-1];
+            dp[i]=Math.max(pick, notPick);
+        }
+        return dp[nums.length-1];
     }
 }
