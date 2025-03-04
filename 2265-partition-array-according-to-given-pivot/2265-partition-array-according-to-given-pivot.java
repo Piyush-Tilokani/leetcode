790d861d2f56529f41facb54c263f972895b00e7
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2025-03-04 17:32:46 +0530

    Time: 4 ms (100%), Space: 59.2 MB (98.96%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,26 +1,28 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> smaller = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
+        int[] smaller = new int[nums.length];
+        int[] greater = new int[nums.length];
         int pivotCount=0;
-        for(int i: nums){
-            if(i==pivot)
+        int i=0;
+        int j=0;
+        for(int n: nums){
+            if(n==pivot)
                 pivotCount++;
-            else if(i<pivot)
-                smaller.add(i);
+            else if(n<pivot)
+                smaller[i++]=n;
             else
-                greater.add(i);
+                greater[j++]=n;
         }
         int []ans = new int[nums.length];
         int ind=0;
-        for(int i: smaller){
-            ans[ind++] = i;
+        for(int k=0; k<i; k++){
+            ans[ind++] = smaller[k];
         }
-        for(int i=0; i<pivotCount; i++){
+        for(int k=0; k<pivotCount; k++){
             ans[ind++] = pivot;
         }
-        for(int i: greater){
-            ans[ind++] = i;
+        for(int k=0; k<j; k++){
+            ans[ind++] = greater[k];
         }
         return ans;       
     }
