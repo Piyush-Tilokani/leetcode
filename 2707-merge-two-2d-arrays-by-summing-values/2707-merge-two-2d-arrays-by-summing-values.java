0c295588958d5ae03e57395e1fcad284b2f288f9
Piyush Tilokani <112542519+Piyush-Tilokani@users.noreply.github.com>
2025-03-17 00:14:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,22 +1,5 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        Map<Integer, Integer> map = new TreeMap<>();
-        for (int[] n : nums1) {
-            if (map.containsKey(n[0]))
-                map.put(n[0], map.get(n[0]) + n[1]);
-            else
-                map.put(n[0], n[1]);
-        }
-        for (int[] n : nums2) {
-            if (map.containsKey(n[0]))
-                map.put(n[0], map.get(n[0]) + n[1]);
-            else
-                map.put(n[0], n[1]);
-        }
-        int[][] ans = new int[map.size()][2];
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet())
-            ans[i++] = new int[] { entry.getKey(), entry.getValue() };
-        return ans;
+        
     }
 }
